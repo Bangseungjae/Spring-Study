e8011da73eb14c8e47b365988203345e1ba6be34
Bangseungjae <tmdwo5825@naver.com>
2022-03-13 16:24:39 +0900

    git test2

--- exception/src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
+++ exception/src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice(basePackages = "hello.exception.api")//git test
+@RestControllerAdvice(basePackages = "hello.exception.api")//git test2
 public class ExControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
